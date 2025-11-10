3c2ebd3088a0040dd533c21ace96beceadd76cf3
juminjae <minjae0812@naver.com>
2025-11-10 18:05:17 +0900

    back-end commit fix chore. 251110

--- src/main/java/likelion13th/shop/global/config/SecurityConfig.java
+++ src/main/java/likelion13th/shop/global/config/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:3000",
                 // "아기 사자 백엔드 배포 주소",
-                "https://james-shop.netlify.app/"
+                "https://james-shop.netlify.app"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
